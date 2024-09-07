ed5a81bef9597940641f805e41e0f3255ccebd3a
Mickaël Lherminez <45120729+mickaellherminez@users.noreply.github.com>
2024-09-07 17:41:26 +0200

    UPDATE: Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Erreur interne du serveur");
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
